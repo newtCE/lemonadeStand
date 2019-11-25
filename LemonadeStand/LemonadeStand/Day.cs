c04d660c889ea7d24695bb366f94e9cb42caf401
Newt Industries <info@newtindustries.com>
2019-11-25 13:57:24 -0600

    improved flow between methods in the day class for determining crowd size, about to feed in static weather before creating the randomizer.

--- LemonadeStand/LemonadeStand/Day.cs
+++ LemonadeStand/LemonadeStand/Day.cs
@@ -9,18 +9,27 @@ namespace LemonadeStand
     class Day
     {
         public string currentDay;
-        int maxCrowd = 12;
-        int currentCrowd;
+        public int currentCrowd;
+        public double currentCrowdDecimal;
+        public int currentTemp;
+        public int tempDifference;
+        public string condition;
+        public int idealTemp = 72;
+        public int maxCrowd = 12;
 
 
-        public int DetermineCurrentCrowd(int temp, string condition, string currentDay)
+
+        public int DetermineCurrentCrowd(int currentTemp, string condition, string currentDay)
         {
             currentCrowd = maxCrowd;
             currentCrowd = ConsiderDay(currentCrowd, currentDay);
             currentCrowd = ConsiderCondition(currentCrowd, condition);
-            currentCrowd = ConsiderTemp(currentCrowd, temp);
+            currentCrowd = ConsiderTemp(currentCrowd, currentTemp);
+            if (currentCrowd < 1)
+            {
+                currentCrowd = 1;//give the player at least one customer on a given day just to be nice
+            }
             return currentCrowd;
-
         }
         public int ConsiderDay(int currentCrowd, string currentDay)
         {
@@ -43,6 +52,7 @@ namespace LemonadeStand
         }
         public int ConsiderCondition(int currentCrowd, string condition)
         {
+            currentCrowdDecimal = currentCrowd;
             switch (condition)
             {
                 case "Sunny":
@@ -50,24 +60,44 @@ namespace LemonadeStand
                     break;
 
                 case "Cloudy":
-                    currentCrowd = currentCrowd* 2;
+                    currentCrowd = Convert.ToInt32(currentCrowdDecimal* .8);
                     break;
 
                 case "Rainy":
+                    currentCrowd = Convert.ToInt32(currentCrowdDecimal * .6);
                     break;
 
                 case "Stormy":
+                    currentCrowd = Convert.ToInt32(currentCrowdDecimal * .4);
                     break;
 
                 case "Snowy":
+                    currentCrowd = Convert.ToInt32(currentCrowdDecimal * .1);
                     break;
             }
             return currentCrowd;
         }
         public int ConsiderTemp(int currentCrowd,int temp)
         {
+            currentCrowdDecimal = currentCrowd; //prepare a decimal for changes made due to temp
+            tempDifference = currentTemp - idealTemp;
+            if (tempDifference == 0)
+            {
+                return currentCrowd;    //no further modifying of crowd needed
+            }
+            else
+            {
+                if (tempDifference < 0)//it's cold remove percentage difference from crowd size
+                {
+                    currentCrowdDecimal = currentCrowdDecimal - (currentCrowdDecimal*((tempDifference * -1) / idealTemp));
+                }
+                else //it's hot add percentage difference from crowd size
+                {
+                    currentCrowdDecimal = currentCrowdDecimal + (currentCrowdDecimal * (tempDifference / idealTemp));
+                }
+            }
+            currentCrowd = Convert.ToInt32(currentCrowdDecimal);
             return currentCrowd;
-        }
         }
     }
 }
