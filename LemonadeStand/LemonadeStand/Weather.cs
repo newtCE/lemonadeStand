961d3af8030d465a0ccf2aa37411a6d03f2f9c0d
Newt Industries <info@newtindustries.com>
2019-11-25 14:32:00 -0600

    created methods with the Weather class

--- LemonadeStand/LemonadeStand/Weather.cs
+++ LemonadeStand/LemonadeStand/Weather.cs
@@ -8,6 +8,28 @@ namespace LemonadeStand
 {
     class Weather
     {
+        public string condition;
+        public int temperature;
+        public List<string> dayList = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        public List<string> conditionList = new List<string>() {"Sunny","Cloudy","Rainy", "Stormy", "Snowy"};
+        public List<string> actualConditions = new List<string>();
+        public List<string> forecastConditions = new List<string>();
+        public List<int> actualTemperatureList = new List<int>();
+        public List<int> forecastTemperatureList = new List<int>();
+
+        public Weather()
+        {
+
+        }
+
+        public string PassTodaysCondition(string currentCondition)
+        {
+            return "Sunny";
+        }
+        public int PassTodaysTemperature(int currentCondition)
+        {
+            return 45;
+        }
 
     }
 }
