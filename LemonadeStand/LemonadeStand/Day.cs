fa6803fe37417546aa93c59ef82daf3442977cba
Newt Industries <info@newtindustries.com>
2019-11-25 12:11:10 -0600

    first code commit. designing classes and working through the process of determining crowd sizes based on weather through the Weather and Day classes

---
+++ LemonadeStand/LemonadeStand/Day.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Day
+    {
+        public string currentDay;
+        int maxCrowd = 12;
+        int currentCrowd;
+
+
+        public int DetermineCurrentCrowd(int temp, string condition, string currentDay)
+        {
+            currentCrowd = maxCrowd;
+            currentCrowd = ConsiderDay(currentCrowd, currentDay);
+            currentCrowd = ConsiderCondition(currentCrowd, condition);
+            currentCrowd = ConsiderTemp(currentCrowd, temp);
+            return currentCrowd;
+
+        }
+        public int ConsiderDay(int currentCrowd, string currentDay)
+        {
+            switch (currentDay)
+            {
+                case "Tuesday":
+                    currentCrowd = currentCrowd + 2;
+                    break;
+
+                case "Friday":
+                    currentCrowd = currentCrowd + 3;
+                    break;
+
+                case "Saturday":
+                case "Sunday":
+                    currentCrowd = currentCrowd + 4;
+                    break;
+            }
+            return currentCrowd;
+        }
+        public int ConsiderCondition(int currentCrowd, string condition)
+        {
+            switch (condition)
+            {
+                case "Sunny":
+                    //no change
+                    break;
+
+                case "Cloudy":
+                    currentCrowd = currentCrowd* 2;
+                    break;
+
+                case "Rainy":
+                    break;
+
+                case "Stormy":
+                    break;
+
+                case "Snowy":
+                    break;
+            }
+            return currentCrowd;
+        }
+        public int ConsiderTemp(int currentCrowd,int temp)
+        {
+            return currentCrowd;
+        }
+        }
+    }
+}
